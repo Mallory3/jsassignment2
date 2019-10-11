c66595de7d7210288c41604313d504a5b9c7ff08
Mallory3 <55147157+Mallory3@users.noreply.github.com>
2019-10-11 14:48:52 -0600

    Finished Styling

--- code/app.js
+++ code/app.js
@@ -1,6 +1,5 @@
 
 
-/* button to click on hamburger menu */
 const menuAppear = document.querySelector(".menu")
 const onClick = document.querySelector('.navMenu')
 menuAppear.addEventListener('click', function() {
@@ -9,7 +8,6 @@ menuAppear.addEventListener('click', function() {
 })
 
 
-/* button to click on X */
 const menuDissappear = document.querySelector(".close")
 const closeClick = document.querySelector('.navMenu')
 menuDissappear.addEventListener('click', function() {
